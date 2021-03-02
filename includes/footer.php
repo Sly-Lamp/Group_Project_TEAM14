672ebf0909a60c74ca399914ec2fcb026c82a767
JakeScottSalt <71704283+JakeScottSalt@users.noreply.github.com>
2021-03-02 15:13:58 +0000

    Add files via upload
    Changed version to 0.0.6

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
     <footer>
-
+     <h4 class="verCtrl">Version: 0.0.6</h4> 
     </footer>
 
-    <script src="./js/scripts.js" />
+    <script src="./js/scripts.js"></script>
 
     </body>
     
