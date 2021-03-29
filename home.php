91ed8d2720851d3db9a384e96cb103e7dd65fe9d
JakeScottSalt <71704283+JakeScottSalt@users.noreply.github.com>
2021-03-29 14:45:09 +0100

    Added updated DB, and editable home page content

--- home.php
+++ home.php
@@ -5,10 +5,75 @@ require './includes/header.php';
 ?>
 
 <div class="container">
-    <h1>Home page</h1>
+    <h1><i class="fas fa-home"></i> Home</h1>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, minima nobis ab ad repellendus eius odio perspiciatis porro eveniet incidunt sequi quia repellat molestiae quisquam officia adipisci amet. Ex, corporis!</p>
+    <?php
 
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            $text = $_POST['txt'];
+            $textErr = "";
+            $t = time();
+            date_default_timezone_set("Europe/London");
+            $updated = date("Y-m-d-H:i:s", $t);
+
+            if(empty(trim($_POST['txt']))){
+                $txtErr = "Please enter some content";
+            } else{
+                $sql = "UPDATE homecontent SET text = ?, updated = ? WHERE id = 1";
+
+                if($stmt = mysqli_prepare($conn, $sql)){
+                    mysqli_stmt_bind_param($stmt, 'ss', $param_text, $param_updated);
+
+                    $param_text = $text;
+                    $param_updated = $updated;
+
+                    if(mysqli_stmt_execute($stmt)){
+                        header('location: home.php?success');
+                    } else{
+                        echo 'Whoops! Something went wrong!';
+                    }
+
+                    // mysqli_stmt_close($stmt);
+                }
+
+                // mysqli_close($conn);
+            }
+    
+
+        }
+
+
+        $sql = "SELECT * FROM homecontent";
+
+        if($result = mysqli_query($conn, $sql)){
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_array($result)){
+                    echo '<p>'.$row['text'].'</p>';
+
+                    if(isset($_SESSION['loggedin']) && $_SESSION['usrType'] >=1){
+                        ?>
+                        <div class="pay">
+                            <form action="home.php" method="post" class="form">
+                                <input type="text" name="txt" id="txt" value="<?php echo $row['text']; ?>">
+                                <button type="submit">Submit</button>
+                            </form>
+                        </div> <?php
+                    }
+                }
+
+                mysqli_free_result($result);
+            
+            } else{ 
+                echo "<p>No records were found.</p>";
+            }
+    
+        } else{
+            echo "ERROR: Could not execute $sql. " . mysqli_error($conn);
+        }
+
+
+    ?>
     
 </div>
 
--- tbd.sql
+++ tbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2021 at 12:26 PM
+-- Generation Time: Mar 29, 2021 at 03:43 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 
@@ -24,10 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `card_details`
+--
+
+CREATE TABLE `card_details` (
+  `id` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `cardNumber` varchar(16) NOT NULL,
+  `securityCode` varchar(3) NOT NULL,
+  `expiry` date NOT NULL,
+  `created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `card_details`
+--
+
+INSERT INTO `card_details` (`id`, `userID`, `cardNumber`, `securityCode`, `expiry`, `created`) VALUES
+(4, 2, '1234567891011121', '456', '2022-03-01', '2021-03-15'),
+(6, 1, '1234567891234567', '878', '0000-00-00', '2021-03-16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `homecontent`
+--
+
+CREATE TABLE `homecontent` (
+  `id` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `homecontent`
+--
+
+INSERT INTO `homecontent` (`id`, `text`, `created`, `updated`) VALUES
+(1, '<p style=\'color:red;\'> Hello 😃 <br><br><br><br><b style=\'color: black;\'>*NOTE:</b> When using inline styles please use single quotes as opposed to double, as it will mess up the scripts!</p>', '2021-03-19 15:30:00', '2021-03-22 15:13:14');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS orders;
 CREATE TABLE `orders` (
   `orderId` int(12) NOT NULL,
   `orderDate` datetime NOT NULL DEFAULT current_timestamp(),
@@ -41,8 +83,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`orderId`, `orderDate`, `usrnm`, `itemId`, `itemName`) VALUES
-(3, '0000-00-00 00:00:00', 'Admin', 2, 'Apple'),
-(4, '2021-02-08 20:09:52', 'Admin', 2, 'grg');
+(3, '0000-00-00 00:00:00', 'Admin', 2, 'Apple');
 
 -- --------------------------------------------------------
 
@@ -50,7 +91,6 @@ INSERT INTO `orders` (`orderId`, `orderDate`, `usrnm`, `itemId`, `itemName`) VAL
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS products;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -68,13 +108,31 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`id`, `name`, `description`, `imageRef`, `price`, `stock`, `created`) VALUES
 (1, 'Apple', 'Robust and crunchy', 'assets/productImages/download.jpg', 100, 250, '2021-02-15'),
 (2, 'Pear', 'Robust and crunchier', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pears.jpg', 100, 250, '2021-02-15'),
-(5, 'Pear', 'Robust and crunchier', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pears.jpg', 100, 250, '2021-02-15'),
-(7, 'Pear', 'Robust and crunchier', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pears.jpg', 100, 250, '2021-02-15'),
-(8, 'Pear', 'Robust and crunchier', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pears.jpg', 100, 250, '2021-02-15'),
-(9, 'Pear', 'Robust and crunchier', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pears.jpg', 100, 250, '2021-02-15'),
-(10, 'Apple', 'Robust and crunchy', 'assets/productImages/download.jpg', 100, 250, '2021-02-15'),
-(11, 'Apple', 'Robust and crunchy', 'assets/productImages/download.jpg', 100, 250, '2021-02-15'),
-(12, 'Apple', 'Robust and crunchy', 'assets/productImages/download.jpg', 100, 250, '2021-02-15');
+(3, 'Papaya', 'More Robust and crunchy', 'https://media.istockphoto.com/photos/two-half-cut-of-ripe-papaya-with-seeds-isolated-on-white-background-picture-id1025189776?s=612x612 6000w, https://media.istockphoto.com/photos/two-half-cut-of-ripe-papaya-with-seeds-isolated-on-white-background-picture', 250, 100, '2021-03-18'),
+(4, 'Strawberries', 'Delicious, but not robust nor crunchy', 'https://i.imgur.com/qyGHL7z.jpg', 500, 500, '0000-00-00'),
+(5, 'Bannaannnnaaaa', 'Edible, but full of potassium. Not reccomended if you have a heart', 'https://i.imgur.com/PvMF3gY.jpg', 500, 500, '2021-03-18'),
+(6, 'What\'s the story...', '...in Balamory? Wouldn\'t you like to know.', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQx48myxnX15p6uM24CKXvYBptXTwdFLsLCSRmNTNd40OBzOc_e', 100, 100, '2021-03-18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pwdreset`
+--
+
+CREATE TABLE `pwdreset` (
+  `pwdResetId` int(11) NOT NULL,
+  `pwdResetEmail` text NOT NULL,
+  `pwdResetSelector` text NOT NULL,
+  `pwdResetToken` longtext NOT NULL,
+  `pwdResetExpires` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pwdreset`
+--
+
+INSERT INTO `pwdreset` (`pwdResetId`, `pwdResetEmail`, `pwdResetSelector`, `pwdResetToken`, `pwdResetExpires`) VALUES
+(22, 'rmcgill@gmail.com', '320513feaa83f785', '$2y$10$mjf3AMeTR77nfZMlG1Jv4uRb1UlzVkZ26oIjG8rdgByuTRUECz8Oa', '1616600712');
 
 -- --------------------------------------------------------
 
@@ -82,57 +140,42 @@ INSERT INTO `products` (`id`, `name`, `description`, `imageRef`, `price`, `stock
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS card_details; -- Foreign key constrain means this needs to happen first
-DROP TABLE IF EXISTS users;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `usrType` int(1) NOT NULL DEFAULT 0,
   `usrnm` varchar(255) NOT NULL,
   `fName` varchar(255) NOT NULL,
   `sName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `pwd` varchar(255) NOT NULL,
-  `created` date NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (id)
+  `created` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`usrType`, `usrnm`, `fName`, `sName`, `email`, `pwd`) VALUES
-(1, 'Admin', 'Group', 'Project', 'email@domain.name', '$2y$10$QoEiVuUCSzxH1mZMZl55cu/GyVwvU3KA5H5ZXNvy.XmFmyR.1mt4G');
-
--- Non-admin sample users
-
-INSERT INTO `users` (`usrType`, `usrnm`, `fName`, `sName`, `email`, `pwd`, `created`) VALUES
-(0, 'Bob', 'Robert', 'McGill', 'rmcgill@gmail.com', '$2y$10$/Pf0ZCTClu2DDezB6XrZBuzQBdbTAAZb4LRr33Y1pKJBSJC6ckyq.', '2021-03-05');
-
---
--- Table structure for table `card_details`
---
-
-CREATE TABLE `card_details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int(11) NOT NULL,
-  `cardNumber` varchar(16) NOT NULL,
-  `securityCode` varchar(3) NOT NULL,
-  `expiry` date NOT NULL,
-  `created` date NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (id),
-  FOREIGN KEY (userID) REFERENCES users(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Sample card details
---
-
-INSERT INTO `card_details` (userID, cardNumber, securityCode, expiry) VALUES
-(2, "1234567891011121", "456", "2022-03-01");
+INSERT INTO `users` (`id`, `usrType`, `usrnm`, `fName`, `sName`, `email`, `pwd`, `created`) VALUES
+(1, 1, 'Admin', 'Group', 'Project', 'email@domain.name', '$2y$10$QoEiVuUCSzxH1mZMZl55cu/GyVwvU3KA5H5ZXNvy.XmFmyR.1mt4G', '2021-02-07'),
+(2, 0, 'Bob', 'Robert', 'McGill', 'rmcgill@gmail.com', '$2y$10$/Pf0ZCTClu2DDezB6XrZBuzQBdbTAAZb4LRr33Y1pKJBSJC6ckyq.', '2021-03-05'),
+(3, 0, 'test', 'Test', 'User', 'test@user.com', '$2y$10$utE5SyP5l4TuL6UQPRwCeuLhF7T2m.cQvPEizetoQNbk5kkZautmS', '2021-03-04');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `card_details`
+--
+ALTER TABLE `card_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `homecontent`
+--
+ALTER TABLE `homecontent`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `orders`
@@ -147,12 +190,32 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `pwdreset`
+--
+ALTER TABLE `pwdreset`
+  ADD PRIMARY KEY (`pwdResetId`);
+
+--
 -- Indexes for table `users`
 --
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `card_details`
+--
+ALTER TABLE `card_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `homecontent`
+--
+ALTER TABLE `homecontent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -166,6 +229,27 @@ ALTER TABLE `orders`
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
+--
+-- AUTO_INCREMENT for table `pwdreset`
+--
+ALTER TABLE `pwdreset`
+  MODIFY `pwdResetId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `card_details`
+--
+ALTER TABLE `card_details`
+  ADD CONSTRAINT `card_details_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
